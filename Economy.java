52ebc9cb7078d68b6c92e5d2546b59ab331dd8a5
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-03-16 13:14:10 +0400

    Create Economy.java

---
+++ Economy.java
@@ -0,0 +1,17 @@
+public class Economy {
+	
+	//economy capacity and price  
+	int eCapacity = 45;
+	int eprice = 200;
+	
+	//economy class constructor 
+	public Economy(int ec, int p){ 
+		  this.eCapacity = ec; 
+		  this.eprice = p;  
+		  }
+
+
+
+
+
+}
