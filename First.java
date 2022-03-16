91688e1f870310660fd7f5573aebf276bc77fb30
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-03-16 13:14:34 +0400

    Create First.java

---
+++ First.java
@@ -0,0 +1,12 @@
+public class First {
+	
+	//first capacity and price 
+	static int fCapacity = 8;
+	static int fprice = 600;
+	
+	public First(int fc, int fp){ 
+		  First.fCapacity = fc; 
+		  First.fprice = fp;  
+		  }
+
+}
