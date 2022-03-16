1e70e740bce21077424b3b5141f935762cf5dd2b
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-03-16 13:12:52 +0400

    Create Business.java

---
+++ Business.java
@@ -0,0 +1,14 @@
+public class Business {
+	
+	//business capacity and price 
+	int bCapacity = 18;
+	int bprice = 300;
+	
+	//business class constructor 
+	public Business(int bc, int bp){ 
+		  this.bCapacity = bc; 
+		  this.bprice = bp;  
+		  }
+
+
+}
