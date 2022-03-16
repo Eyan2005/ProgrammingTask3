11c7d617e471025b6484102790d5b1be08ea97cc
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-03-16 13:13:39 +0400

    Create Destinations.java

---
+++ Destinations.java
@@ -0,0 +1,8 @@
+public class Destinations {
+	
+	//destinations list 
+	static final String[] destinations = new String[] {"Manchester", "Toronto", 
+			"Male", "London", "Dubai", "New York", "Beirut", "Los Angeles", 
+			"Istanbul", "Kuala Lumpur"};
+
+}
